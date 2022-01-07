2253263c02421b97581d933ac1a66e5ef8d75666
sollyu <sollyu@qq.com>
2022-01-07 10:44:30 +0800

    :tada: 海内存知己 天涯若比邻

--- src/js/itask.ts
+++ src/js/itask.ts
@@ -1,53 +1,71 @@
-export class TaskBean {
+class TaskBean {
     title: AccessibilityNodeInfo
     desc: AccessibilityNodeInfo
     button: AccessibilityNodeInfo
 }
 
-export interface ITask {
+interface ITask {
     execute(): void;
 }
 
-export class TaskWatchVideo implements ITask {
-    private mTaskBean: TaskBean
-
-    constructor(item: TaskBean) {
-        this.mTaskBean = item
+/**
+ * 基础页面任务类型
+ */
+abstract class TaskPageBase implements ITask {
+    execute() {
+        this.doExecute();
+        this.doBack();
     }
-
-    execute(): void {
-        console.log('执行任务：' + this.mTaskBean.title.text, this.mTaskBean.button.text);
-        this.mTaskBean.button.click();
-        assistant.thread.sleep(1000);       // 1 秒的反应时间
-        assistant.thread.sleep(15 * 1000);  // 等待15秒的视频观看时间
-        assistant.accessibility.find({id: 'com.xiaopeng.mycarinfo:id/btn_back', class: 'android.widget.ImageView'})[0].click();
-    }
-}
-
-abstract class TaskWebPage implements ITask {
 
     protected abstract doExecute();
 
-    execute(): void {
-        this.doExecute();
-        this.executeBack();
-    }
+    protected abstract doBack();
+}
 
-    protected executeBack() {
+/**
+ * 基础网页风格类型1
+ */
+abstract class TaskWebPage1 extends TaskPageBase {
+    protected doBack() {
         let content = assistant.accessibility.find({id: 'android:id/content', class: 'android.widget.FrameLayout'})[0];
         let backBtn = content.child(0).child(0).child(0).child(0).child(0).child(0).child(1).child(0);
         backBtn.click()
     }
 }
 
-export class TaskPostRead extends TaskWebPage {
+/**
+ * 基础网页风格类型2
+ */
+abstract class TaskWebPage2 extends TaskPageBase {
+    protected doBack() {
+        assistant.accessibility.find({id: 'com.xiaopeng.mycarinfo:id/iv_back_icon', class: 'android.widget.ImageView'})[0].click();
+    }
+}
+
+/**
+ * 基础网页风格类型2
+ */
+abstract class TaskWebPage3 extends TaskPageBase {
+    protected doBack() {
+        assistant.accessibility.find({id: 'com.xiaopeng.mycarinfo:id/btn_back', class: 'android.widget.ImageView'})[0].click();
+    }
+}
+
+class TaskPostRead extends TaskWebPage1 {
     protected doExecute() {
         assistant.thread.sleep(1000);       // 1 秒的反应时间
         assistant.thread.sleep(15 * 1000);  // 等待15秒的视频观看时间
     }
 }
 
-export class TaskPostFollow extends TaskWebPage {
+class TaskPostRead2 extends TaskWebPage2 {
+    protected doExecute() {
+        assistant.thread.sleep(1000);       // 1 秒的反应时间
+        assistant.thread.sleep(15 * 1000);  // 等待15秒的视频观看时间
+    }
+}
+
+class TaskPostFollow extends TaskWebPage1 {
     protected doExecute() {
         assistant.accessibility.find({text: '关注', class: 'android.widget.TextView'})[0].click();
         assistant.accessibility.find({text: '已关注', class: 'android.widget.TextView'})[0].click();
@@ -55,7 +73,7 @@ export class TaskPostFollow extends TaskWebPage {
     }
 }
 
-export class TaskPostThumbUp extends TaskWebPage {
+class TaskPostThumbUp extends TaskWebPage1 {
     protected doExecute() {
         assistant.accessibility.find({text: '用观点表明你的态度', class: 'android.widget.TextView'})[0].parent().parent().child(2).click();
         assistant.thread.sleep(2000);
@@ -63,16 +81,17 @@ export class TaskPostThumbUp extends TaskWebPage {
     }
 }
 
-export class TaskPostRead2 extends TaskPostRead {
-    protected executeBack() {
-        assistant.accessibility.find({id: 'com.xiaopeng.mycarinfo:id/iv_back_icon', class: 'android.widget.ImageView'})[0].click();
+class TaskWatchVideo extends TaskWebPage3 {
+    protected doExecute() {
+        assistant.thread.sleep(1000);       // 1 秒的反应时间
+        assistant.thread.sleep(15 * 1000);  // 等待15秒的视频观看时间
     }
 }
 
 /**
  * 领取我的奖品
  */
-export class TaskCashingPrize extends TaskPostRead2 {
+export class TaskCashingPrize extends TaskWebPage2 {
     protected doExecute() {
         assistant.accessibility.all().filter(item => item.text == 'animation' && item.class == 'android.widget.Button')[0].parent().child(2).click();
         assistant.accessibility.wait({text: '一键领取', class: 'android.widget.TextView'})[0].click();
@@ -85,5 +104,67 @@ export class TaskCashingPrize extends TaskPostRead2 {
  */
 export class TaskClearNotify implements ITask {
     execute(): void {
+    }
+}
+
+/**
+ * 每日任务的Web界面，每次执行一个任务
+ */
+export class TaskWebDailyPage extends TaskWebPage1 {
+    protected doExecute() {
+        assistant.accessibility.wait({text: '每日任务', class: 'android.widget.TextView'})[0].click();
+        assistant.thread.sleep(1000);
+        let executeTaskBean: TaskBean = null;
+        const ignoreLottery: boolean = 1 - 1 == 0;        // 是否忽略抽奖
+        const nodeInfos = assistant.accessibility.all()
+        for (let i = 0; i < nodeInfos.length; i++) {
+            let item = nodeInfos[i];
+            if (item.text == '去完成') {
+                if (ignoreLottery == true && nodeInfos[i - 2].text == '每日抽奖') {
+                    continue;
+                }
+                executeTaskBean = {title: nodeInfos[i - 2], desc: nodeInfos[i - 1], button: nodeInfos[i]};
+                break;
+            }
+        }
+
+        if (executeTaskBean == null) {
+            throw new Error('全部任务完成');
+        }
+
+        console.log(executeTaskBean.title.text);
+
+        let iTask: ITask = null;
+        switch (executeTaskBean.title.text) {
+            case '为什么选择小鹏汽车':
+                executeTaskBean.button.click();
+                iTask = new TaskWatchVideo();
+                break;
+            case '玩转鹏友生活':
+                executeTaskBean.button.click();
+                iTask = new TaskPostRead();
+                break;
+            case '逛一逛门店':
+            case '看一看二手车页面':
+            case '小鹏精选好物':
+                executeTaskBean.button.click();
+                iTask = new TaskPostRead2();
+                break;
+            case '文章点赞':
+                executeTaskBean.button.click();
+                iTask = new TaskPostThumbUp();
+                break;
+            case '关注一个用户':
+                executeTaskBean.button.click();
+                iTask = new TaskPostFollow();
+                break;
+            case '每日抽奖':
+                break;
+        }
+
+        if (iTask != null) {
+            iTask.execute();
+        }
+
     }
 }
--- src/js/main.ts
+++ src/js/main.ts
@@ -1,71 +1,27 @@
-import {ITask, TaskBean, TaskCashingPrize, TaskClearNotify, TaskPostFollow, TaskPostRead, TaskPostRead2, TaskPostThumbUp, TaskWatchVideo} from "./itask"
+import {TaskCashingPrize, TaskClearNotify, TaskWebDailyPage} from "./itask"
 
 if (assistant.app.isInstalled('com.xiaopeng.mycarinfo') == false)
     throw new Error("请先安装小鹏汽车");
+
 assistant.app.run('com.xiaopeng.mycarinfo');
 assistant.accessibility.waitActivity('com.xiaopeng.mycarinfo/.business.home.HomeActivity');
 assistant.accessibility.wait({id: 'com.xiaopeng.mycarinfo:id/tv_tab_text', text: '我的', class: 'android.widget.TextView'})[0].click();
 assistant.accessibility.wait({id: 'com.xiaopeng.mycarinfo:id/item_title', text: '任务中心', class: 'android.widget.TextView'})[0].click();
 assistant.thread.sleep(1000);
 
-function executeSingleTask() {
-    assistant.accessibility.wait({text: '每日任务', class: 'android.widget.TextView'})[0].click();
-    assistant.thread.sleep(1000);
-    let executeTaskBean: TaskBean = null;
-    const ui = assistant.accessibility.all()
-    for (let i = 0; i < ui.length; i++) {
-        let item = ui[i];
-        if (item.text == '去完成') {
-            console.log(ui[i + 3].text, ui[i + 2].text, ui[i + 1].text, ui[i].text, ui[i - 1].text, ui[i - 2].text, ui[i - 3].text,)
-            executeTaskBean = {title: ui[i - 2], desc: ui[i - 1], button: ui[i]};
-            break;
-        }
-    }
-
-    if (executeTaskBean == null) {
-        console.log('全部任务完成');
-        return;
-    }
-
-    console.log(executeTaskBean.title.text);
-
-    let iTask: ITask = null;
-    switch (executeTaskBean.title.text) {
-        case '为什么选择小鹏汽车':
-            iTask = new TaskWatchVideo(executeTaskBean);
-            break;
-        case '玩转鹏友生活':
-            executeTaskBean.button.click();
-            iTask = new TaskPostRead();
-            break;
-        case '逛一逛门店':
-            executeTaskBean.button.click();
-            iTask = new TaskPostRead2();
-            break;
-        case '文章点赞':
-            executeTaskBean.button.click();
-            iTask = new TaskPostThumbUp();
-            break;
-        case '关注一个用户':
-            executeTaskBean.button.click();
-            iTask = new TaskPostFollow();
-            break;
-    }
-
-    if (iTask != null) {
-        iTask.execute();
-    }
+try {
+    new TaskWebDailyPage().execute();
+    new TaskWebDailyPage().execute();
+    new TaskWebDailyPage().execute();
+    new TaskWebDailyPage().execute();
+    new TaskWebDailyPage().execute();
+} catch (e) {
+    console.error(e);
 }
-
-executeSingleTask();
-// executeSingleTask();
-// executeSingleTask();
-// executeSingleTask();
-// executeSingleTask();
 
 // 领取我的奖金
 new TaskCashingPrize().execute();
 
 // 清空通知消息
-new TaskClearNotify().execute();
+//new TaskClearNotify().execute();
 
